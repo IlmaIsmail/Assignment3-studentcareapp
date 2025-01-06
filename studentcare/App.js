99b0a20053aba399d23d3bd49799fc14b07898ec
Ilma Ismail <ilmaismail029@gmail.com>
2025-01-06 14:32:37 +0530

    importing components to app file

--- studentcare/App.js
+++ studentcare/App.js
@@ -1,1 +1,12 @@
- 
+import React from "react";
+import { StyleSheet, View, StatusBar, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+
+const Stack = createStackNavigator();
+
+export default function App() {}
